833671ca04fa6956e7100dc58503cbedb8e43480
BheemaBadri <badrib@gene.com>
2024-04-19 09:03:12 +0530

    First Commit

---
+++ Practice_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice_Swift
+//
+//  Created by Admin on 19/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
